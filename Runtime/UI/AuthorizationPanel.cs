b9f07870ab3cae37c72cb1681971829c700a99ee
scrawach <scrawach@gmail.com>
2024-06-03 23:19:47 +0300

    introduced authorization panel script with UI toolkit components

---
+++ Runtime/UI/AuthorizationPanel.cs
@@ -0,0 +1,17 @@
+﻿namespace Plugins.AuthService.Runtime.UI
+{
+    public class AuthorizationPanel
+    {
+        
+    }
+
+    public class SignUpPanel
+    {
+        
+    }
+
+    public class SignInPanel
+    {
+        
+    }
+}
